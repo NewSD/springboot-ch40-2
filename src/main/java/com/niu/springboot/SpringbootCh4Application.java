eb9aa3c720b946750e8aacbd20e0540706aed558
18300603955 <oceaniu@163.com>
2019-06-12 21:26:38 +0800

    exclude

--- src/main/java/com/niu/springboot/SpringbootCh4Application.java
+++ src/main/java/com/niu/springboot/SpringbootCh4Application.java
@@ -1,15 +1,19 @@
 package com.niu.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @ServletComponentScan
+//除去某个自动配置
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class SpringbootCh4Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootCh4Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCh4Application.class, args);
+    }
 
 }
