ae94077b06a7a89439b37409cc87c3424c411157
18300603955 <oceaniu@163.com>
2019-06-12 21:00:48 +0800

    path map

--- src/main/java/com/niu/springboot/config/WebMvcConfig.java
+++ src/main/java/com/niu/springboot/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.niu.springboot.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -14,5 +15,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(new MyInterceptor1())
                 .addPathPatterns("/**")
                 .excludePathPatterns("/hello");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/index").setViewName("index");
     }
 }
