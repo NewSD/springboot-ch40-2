7661cc9f5fe10f05cef0aae1efd794efea797934
18300603955 <oceaniu@163.com>
2019-06-12 20:53:49 +0800

    dopost

--- src/main/java/com/niu/springboot/servlet/MyServlet.java
+++ src/main/java/com/niu/springboot/servlet/MyServlet.java
@@ -14,12 +14,11 @@ import java.io.IOException;
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        doPost(req,resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        super.doPost(req, resp);
         System.out.println("name:>>>"+req.getParameter("name"));
     }
 
